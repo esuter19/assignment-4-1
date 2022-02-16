126ab3733a5fd2ced26bc873dd6ba61919acbd79
esuter19 <59849377+esuter19@users.noreply.github.com>
2022-02-16 00:15:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,100 @@
-require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
-        var map = new Map({
-          basemap: "topo-vector"
-        });
 
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 4,
-          center: [15, 65] // longitude, latitude
+    require([
+      "esri/WebScene",
+      "esri/views/SceneView",
+      "esri/Camera",
+      "esri/widgets/Home",
+      "dojo/domReady!"
+    ], function(WebScene, SceneView, Camera, Home) {
+
+    
+      /*var map = new Map({
+        basemap: "streets",
+        ground: "world-elevation"
+      });*/
+      var scene = new WebScene({
+        portalItem:{
+         id:"8046207c1c214b5587230f5e5f8efc77" 
+        }
+      });
+      
+      var camera = new Camera({
+        position: [
+          -71.00933050966589,
+          42.364475930262344,
+          5000// elevation in meters
+        ],
+        tilt:0,
+        heading: 0
+      })
+      
+      var camera2 = new Camera({
+        position: {
+          x: -71.0969755617771,
+          y: 42.346566295251115,
+          z: 2500
+        },
+        tilt: 0,
+        heading: 0
+      });
+      
+      var camera3 = new Camera({
+        
+      });
+
+      var view = new SceneView({
+        container: "viewDiv",
+        map: scene,
+        viewingMode:"global",
+        camera: camera,
+        environment: {
+            lighting: {
+              date: new Date(),
+              directShadowsEnabled: true,
+              // don't update the view time when user pans.
+              // The clock widget drives the time
+              cameraTrackingEnabled: false
+            }
+        },
+    });
+    
+    var homeBtn = new Home({
+        view: view
+      });
+
+      // Add the home button to the top left corner of the view
+    view.ui.add(homeBtn, "top-left");
+    
+    [airport, fenway, coast].forEach(function(button) {
+      button.style.display = 'flex';
+      view.ui.add(button, 'top-right');
+    });
+    
+    fenway.addEventListener('click', function() {
+      // reuse the default camera position already established in the homeBtn
+      view.goTo({
+        target:camera2
+      });
+    });
+    
+    airport.addEventListener('click', function() {
+      // reuse the default camera position already established in the homeBtn
+      view.goTo({
+        target:camera
+      });
+    });
+      
+      coast.addEventListener('click',function(){
+        view.goTo({
+          position: {
+          x: -70.97948322283244,
+          y: 42.34074965401074,
+          z: 1000
+        },
+        heading: -80,
+        tilt: 75
         });
       });
+
+
+    });
